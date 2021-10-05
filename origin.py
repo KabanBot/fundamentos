f2a1a8acc0f2f5683156a8c590f5f6b8e87f62ae
KabanBot <eevee2131@gmail.com>
2021-10-04 20:24:05 -0700

    Cadena de python

---
+++ origin.py
@@ -0,0 +1,15 @@
+name='tomas miguel rodriguez moreno'
+age=10
+istall=True
+height=1.80
+print(name+" Tiene "+str(age)+" años ")
+print("{} tiene {} años".format(name,age))
+
+print(f"{name} Tiene {age+10} años y es \
+    Alto {istall} y mide {height}")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name*5)
+
